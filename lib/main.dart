8c3af4a81f7e645c67773343c90bbbb3fc2942de
lhaislla <lhaislla@gmail.com>
2022-04-18 00:10:37 -0300

    armazenamento de  dados no Firebase Firestore Database

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
   runApp(const MyApp());
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: 'AIzaSyAaFo6b1cvDK2N8kYZntQ4DIk_hsq5Ciis',
+          appId: '1:889939815861:android:888bf87a761dc461027dfc',
+          messagingSenderId: '889939815861',
+          projectId: 'palavras-7a041'));
   FirebaseFirestore.instance
       .collection("Parpalavra")
       .doc("palavra")
